32b1db533f944e6e0ad1d99909513fd8b4daebaa
Chandler Tayek <chandlertayek@gmail.com>
2017-01-08 21:08:17 -0800

    feat: Complete regularized cost and regression

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -9,7 +9,8 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
-grad = zeros(size(theta));
+b = size(theta); 
+grad = zeros(b);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -17,7 +18,15 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+z = theta'*X';
+h_theta = sigmoid(z);
+J = (1/m)*(log(h_theta)*-y - log(1 - h_theta)*(1 - y)) + lambda/(2*m) * (theta(2:b)')*theta(2:b);
 
+% for J(1) we don't regularize
+grad(1) = X(:,1)'*(1/m)*(h_theta' - y);
+
+% Regularized gradient for J through number of features
+grad(2:b) = X(:,(2:b))'*(1/m)*(h_theta' - y)  + lambda/m * theta(2:b);
 
 
 
